09f7644f6a23f948e5b48157d77c3a9f36984883
paatrickreis <paatrick_reis@hotmail.com>
2017-06-13 15:46:26 -0300

    Aleteraçoes

--- gestaoapp/models/edital.py
+++ gestaoapp/models/edital.py
@@ -6,9 +6,10 @@ class Edital(models.Model):
     orgao_concedente = models.CharField(max_length=255)
     dt_inicio = models.DateField()
     dt_termino = models.DateField()
-    url_edital = models.URLField(max_length=200)
-    verba = models.FloatField()
-    qtd_bolsa = models.IntegerField()
+    url_edital = models.URLField(max_length=200, null=True, blank=True)
+    verba = models.FloatField(null=True, blank=True)
+    qtd_bolsa = models.IntegerField( null=True, blank=True)
+    pdf_edital = models.FileField(null=True, blank=True)
 
     def __unicode__(self):
         return unicode(str(self.numero))
