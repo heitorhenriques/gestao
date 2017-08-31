604394b50399ada1d60fba7c69270d40b9e9023b
flamarion <felipeflamari@gmail.com>
2017-08-31 12:51:16 -0300

    Botão redirecionando para o login após o cadastro

--- gestaoapp/views/home.py
+++ gestaoapp/views/home.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.base import View
 import logging
@@ -9,12 +10,11 @@ from gestaoapp.models.projeto import Projeto
 class Home(View):
     template = 'home/index.html'
 
-
     def get(self, request):
-        #logger = logging.getLogger('__name__')
-        #logger.info('Testando Django Monitoring Splunk!')
-        projeto_membro = Projeto.objects.filter(membro=request.user.id)
-        projeto_coordenador = Projeto.objects.filter(coordenador=request.user.id)
+        if request.user.is_authenticated():
+            projeto_membro = Projeto.objects.filter(membro=request.user.id)
+            projeto_coordenador = Projeto.objects.filter(coordenador=request.user.id)
 
-        return render(request, self.template,
-                      {'projeto_membro': projeto_membro, 'projeto_coordenador': projeto_coordenador})
+            return render(request, self.template, {'projeto_membro': projeto_membro, 'projeto_coordenador': projeto_coordenador})
+        else:
+            return HttpResponseRedirect('/login/')
